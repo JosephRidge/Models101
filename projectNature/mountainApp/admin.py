24a692804f7feafa5a7ab1f4966a7b6c357b6324
JosephRidge <josephridge18@gmail.com>
2026-03-13 10:32:08 +0300

    model creation

--- projectNature/mountainApp/admin.py
+++ projectNature/mountainApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Mountain, HikeEvent
 
 # Register your models here.
+admin.site.register(Mountain)
+admin.site.register(HikeEvent)
+
--- projectNature/mountainApp/models.py
+++ projectNature/mountainApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class HikeEvent(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    date = models.DateTimeField()
+    venue = models.CharField(max_length=150)
+    arrival_time =models.TimeField()
+    image_link = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Mountain(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    height = models.FloatField()
+    isVolcanic = models.BooleanField(default=False)
+    image_link = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
